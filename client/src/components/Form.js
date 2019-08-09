5a0cf3974cda3e784399e1053c27008bd64c47c5
Reese Kunz <reesekunz21@gmail.com>
2019-08-09 08:32:19 -0700

    built out username and password form inputs with formik

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,22 +1,46 @@
-import React from 'react';
-import axios from 'axios';
-// import {Form, Field, withFormik} from 'formik';
+import React from "react";
+import axios from "axios";
+import {Form, Field, withFormik} from 'formik';
 // import * as Yup from 'yup';
 
 class UserForm extends React.Component {
-constructor (){
-super();
-this.state = {
-users: [],
-}
-}
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    };
+  }
 
+  render() {
+    return (
+      <div className="user-form">
+        <h1>User Form</h1>
+        <Form>
+          {/* username input  */}
+          <div className="input">
+            <h3 className="header">username: </h3>
+            <Field type="text" name="username" placeholder="username" />*
+            {/* {props.touched.username && props.errors.username && (
+              <p>{props.errors.username}</p>
+            )} */}
+            </div>
 
-render() {
-return (
-<div>Form Placeholder</div>
-)
-}
+        {/* // password input  */}
+        <div className="input">
+        <h3 className="header">Password: </h3>
+        <Field type="password" name="password" placeholder="password" />*
+        {/* {props.touched.password && props.errors.password && (
+          <p>{props.errors.password}</p>
+        )} */}
+        </div>
+
+         {/* // submit button  */}
+         <button className="button" type="submit"> Submit!</button>
+
+        </Form>
+      </div>
+    );
+  }
 }
 
 export default UserForm;
