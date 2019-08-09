ba229dbc3dc8f601d6bfaa90b812addd261488ad
Reese Kunz <reesekunz21@gmail.com>
2019-08-09 14:25:27 -0700

    added testing, finished mvp

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,38 +1,40 @@
   import React from "react";
-  
-  import * as rtl from '@testing-library/react';
-  import 'jest-dom/extend-expect';
-  
+  import ReactDOM from 'react-dom';
+  // importing from react-testing-library
+  import {render, fireEvent} from '@testing-library/react'
+  //cleanup 
+  import "@testing-library/react/cleanup-after-each";  
+  //imports 
   import App from "./App";
+  import GetData from './components/DataRetrieval'
+  import FormikUserForm from './components/Form.js'
+  
+  describe("<App />", () => {
 
-  afterEach(rtl.cleanup);
+    it("renders without crashing using ReactDOM", () => {
+      const div = document.createElement("div");
+      ReactDOM.render(<App />, div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+  
+    it("renders Form  without crashing", () => {
+      
+      render(<FormikUserForm />);
+    });
 
-  it('renders without crashing', () => {
-    const wrapper = rtl.render(
-      <span className="user-form">User Form</span>
-    );
+    it('clicking button', () => {
+      const form = render(<FormikUserForm />)
+      const button = form.getByText(/submit/i)
+      fireEvent.click(button)
+    })
+  
+    // it("renders User Form text", () => {
+    //   const app = render(<App />);
+  
+    //   app.getByText(/User Form/i);
+    // });
+
   });
-
-  
-  // describe("<App />", () => {
-
-  //   it("renders without crashing using ReactDOM", () => {
-  //     const div = document.createElement("div");
-  //     ReactDOM.render(<App />, div);
-  //     ReactDOM.unmountComponentAtNode(div);
-  //   });
-  
-  //   it("renders without crashing", () => {
-  //     render(<App />);
-  //   });
-  
-  //   it("renders User Form text", () => {
-  //     const app = render(<App />);
-  
-  //     app.getByText(/User Form/i);
-  //   });
-
-  // });
 
 
 // Other expect matchers we could use:
