11bcd9b776a58ebf855b17d8a214bbaffca44b2c
Reese Kunz <reesekunz21@gmail.com>
2019-08-09 08:49:28 -0700

    fixed export default issue

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,13 +1,14 @@
 import React from "react";
 import axios from "axios";
-import {Form, Field, withFormik} from 'formik';
-import * as Yup from 'yup';
+import { Form, Field, withFormik } from "formik";
+import * as Yup from "yup";
 
 class UserForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      users: []
+      name: " ",
+      password: " "
     };
   }
 
@@ -19,63 +20,60 @@ class UserForm extends React.Component {
           {/* username input  */}
           <div className="input">
             <h3 className="header">username: </h3>
-            <Field type="text" name="username" placeholder="username" />*
-            {props.touched.username && props.errors.username && (
-              <p>{props.errors.username}</p>
-            )}
-            </div>
+            <Field type="text" name="name" placeholder="username" />*
+            {/* {this.props.touched.name && this.props.errors.name && (
+                <p>{this.props.errors.name}</p>
+            )} */}
+          </div>
 
-        {/* // password input  */}
-        <div className="input">
-        <h3 className="header">Password: </h3>
-        <Field type="password" name="password" placeholder="password" />*
-        {props.touched.password && props.errors.password && (
-          <p>{props.errors.password}</p>
-        )}
-        </div>
+          {/* // password input  */}
+          <div className="input">
+            <h3 className="header">Password: </h3>
+            <Field type="password" name="password" placeholder="password" />*
+            {/* {this.props.touched.password && this.props.errors.password && (
+          <p>{this.props.errors.password}</p>
+        )} */}
+          </div>
 
-         {/* // submit button  */}
-         <button className="button" type="submit"> Submit!</button>
-
+          {/* // submit button  */}
+          <button className="button" type="submit">
+            {" "}
+            Submit!
+          </button>
         </Form>
       </div>
     );
   }
 }
 
-// Higher Order Component - returns copy of UserForm but w/ extended logic 
+// Higher Order Component - returns copy of UserForm but w/ extended logic
 const FormikUserForm = withFormik({
-    mapPropsToValues(values) {
-      return {
-        username: values.username || "",
-        password: values.password || "",
-      };
-    },
+  mapPropsToValues(values) {
+    return {
+      name: values.name || "",
+      password: values.password || ""
+    };
+  },
 
-    // Yup form validation 
-    // these give you the error props you need to apply under each corresponding <Field> input
-    validationSchema: Yup.object().shape({
-        name: Yup.string().required(),
-        password: Yup.string()
-          .min(6)
-          .required(),
-      }),
+  // Yup form validation
+  // these give you the error props you need to apply under each corresponding <Field> input
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required(),
+    password: Yup.string()
+      .min(6)
+      .required()
+  }),
 
+  handleSubmit(values, { setStatus, resetForm }) {
+    axios
+      .post("http://localhost:5000/api/register", values)
+      .then(response => {
+        console.log(response);
+        setStatus(response.data);
+      })
+      .catch(error => console.log(error.response));
+    resetForm(); // form reset on submit
+  }
+})(UserForm); // currying functions in Javascript
 
-    handleSubmit(values, { setStatus, resetForm }) {
-      axios
-        .post("http://localhost:5000/api/register", values)
-        .then(response => {
-          console.log(response);
-          setStatus(response.data);
-        })
-        .catch(error => console.log(error.response));
-        resetForm(); // form reset on submit 
-    }
-  })(UserForm); // currying functions in Javascript
-  
-
-
-
-
-export default UserForm;
+export default FormikUserForm;
