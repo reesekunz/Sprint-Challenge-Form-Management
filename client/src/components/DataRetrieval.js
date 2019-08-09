21f703fba6a832b25ccd408cc7d469342de31094
Reese Kunz <reesekunz21@gmail.com>
2019-08-09 10:25:31 -0700

    cleaned up data component

--- client/src/components/DataRetrieval.js
+++ client/src/components/DataRetrieval.js
@@ -1,49 +1,45 @@
-import React from 'react';
+import React from "react";
 
-import axios from 'axios';
+import axios from "axios";
 
 class GetData extends React.Component {
-    constructor() {
-      super();
-      this.state = {
-        users: [ ],
-      };
-    }
+  constructor() {
+    super();
+    this.state = {
+      meals: []
+    };
+  }
 
-    componentDidUpdate(prevProps, prevState) {
-        if (prevState.user !== this.state.user) {
-          this.fetchusers();
-        }
-    }
-    
-    componentDidMount() {
-        axios.get(`http://localhost:5000/api/restricted/data`)
-          .then(response => {
-            const users = response.data;
-            this.setState({ users });
-          })
-      }
+  //   componentDidUpdate(prevProps, prevState) {
+  //     if (prevState.meal !== this.state.meal) {
+  //       this.fetchMeals();
+  //     }
+  //   }
 
+  componentDidMount() {
+    axios.get(`http://localhost:5000/api/restricted/data`).then(response => {
+      const meals = response.data;
+      this.setState({ meals });
+    });
+  }
 
-      render() {
-          console.log(this.state)
-        return (
-          <ul>
-            { this.state.users.map(user => 
-            <div className="recipe">
-            <div className="meal">Meal: {user.name}</div>
-            <div className="course">Course: {user.course}</div>
-            <div className="technique">Technique: {user.technique}</div>
-            <div className="ingredients">Ingredients: {user.ingredients}</div>
-            
-            </div>
+  render() {
+    console.log(this.state);
+    console.log(this.state.meals);
 
-            )}
-          </ul>
-        )
-      }
-    }
-      
-
+    return (
+      <ul>
+        {this.state.meals.map(meal => (
+          <div className="recipe" key={meal.name}>
+            <div className="meal">Meal: {meal.name}</div>
+            <div className="course">Course: {meal.course}</div>
+            <div className="technique">Technique: {meal.technique}</div>
+            <div className="ingredients">Ingredients: {meal.ingredients}</div>
+          </div>
+        ))}
+      </ul>
+    );
+  }
+}
 
 export default GetData;
