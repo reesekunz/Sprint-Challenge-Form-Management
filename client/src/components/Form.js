5b7e8d570c5ff20de81c95708754cfb98ca69a27
Reese Kunz <reesekunz21@gmail.com>
2019-08-09 08:24:41 -0700

    set up class template

---
+++ client/src/components/Form.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import axios from 'axios';
+// import {Form, Field, withFormik} from 'formik';
+// import * as Yup from 'yup';
+
+class UserForm extends React.Component {
+constructor (){
+super();
+this.state = {
+users: [],
+}
+}
+
+
+render() {
+return (
+<div>Form Placeholder</div>
+)
+}
+}
+
+export default UserForm;
